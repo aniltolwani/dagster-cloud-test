432923d9ff4c173023493b221255e4a294220f23
Anil Tolwani <anil@unlearn.ai>
2023-10-10 23:54:17 -0700

    add sensor to the init

--- quickstart_etl/__init__.py
+++ quickstart_etl/__init__.py
@@ -4,13 +4,18 @@ from dagster import (
     define_asset_job,
     load_assets_from_package_module,
 )
-
 from . import assets
+from .sensors.github import make_github_prs_updated_sensor
 
-daily_refresh_schedule = ScheduleDefinition(
-    job=define_asset_job(name="all_assets_job"), cron_schedule="0 0 * * *"
-)
+# Define your job
+job = define_asset_job(name="all_assets_job")
 
+
+# Define your sensor
+github_prs_updated_sensor = make_github_prs_updated_sensor(job)
+
+# Add your assets, schedules, and sensors to your Definitions
 defs = Definitions(
-    assets=load_assets_from_package_module(assets), schedules=[daily_refresh_schedule]
+    assets=load_assets_from_package_module(assets),
+    sensors=[github_prs_updated_sensor],
 )
