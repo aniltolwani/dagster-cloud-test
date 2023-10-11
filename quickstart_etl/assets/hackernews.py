05233f6bf80f1bc6d601d7860af2b4c8786981f0
Anil Tolwani <anil@unlearn.ai>
2023-10-10 21:55:34 -0700

    one more

--- quickstart_etl/assets/hackernews.py
+++ quickstart_etl/assets/hackernews.py
@@ -41,6 +41,7 @@ def hackernews_topstories(
     df['new_col'] = 'A'
     df['new_col_B'] = 'B'
     df['new_col_C'] = 'C'
+    df['new_col_D'] = 'D'
 
     # Dagster supports attaching arbitrary metadata to asset materializations. This metadata will be
     # shown in the run logs and also be displayed on the "Activity" tab of the "Asset Details" page in the UI.
