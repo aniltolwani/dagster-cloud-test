a7d0229447f02e52488240f71f99096375fe29ee
Anil Tolwani <anil@unlearn.ai>
2023-10-10 23:48:30 -0700

    different sensor approach

--- quickstart_etl/sensors/github.py
+++ quickstart_etl/sensors/github.py
@@ -1,17 +1,37 @@
-from dagster import sensor, RunRequest
-from dagster.core.definitions.decorators.sensor import pipeline_failure_sensor
-from dagster.core.definitions.pipeline_sensor import PipelineFailureSensorContext
-from dagster.core.storage.pipeline_run import PipelineRunsFilter
-from dagster.utils import merge_dicts
-from dagster_github import github_resource
-from dagster import repository
 
-@sensor(pipeline_name="my_pipeline")
-def my_github_sensor(context):
-    github = context.resources.github
+import json
+from dagster import (
+    AssetKey,
+    DagsterEventType,
+    EventRecordsFilter,
+    RunRequest,
+    SensorDefinition,
+    sensor,
+)
 
-    # replace "aniltolwani/dagster-cloud-test" with your repo
-    result = github.get("/repos/aniltolwani/dagster-cloud-test/pulls")
+def make_github_prs_updated_sensor(job) -> SensorDefinition:
+    """Returns a sensor that launches the given job when the GitHub "pull requests" have been updated."""
 
-    for pr in result:
-        yield RunRequest(run_key=pr["id"], run_config={})
+    @sensor(name=f"{job.name}_on_github_prs_updated", job=job)
+    def github_prs_updated_sensor(context):
+        cursor = context.cursor if context.cursor else None
+
+        pr_event_records = context.instance.get_event_records(
+            EventRecordsFilter(
+                event_type=DagsterEventType.ASSET_MATERIALIZATION,
+                asset_key=AssetKey(["github", "core", "pull_requests"]),
+                after_cursor=cursor,
+            ),
+            ascending=False,
+            limit=1,
+        )
+
+        if not pr_event_records:
+            return
+
+        yield RunRequest(run_key=None)
+
+        # update the sensor cursor with the latest event cursor
+        context.update_cursor(json.dumps({"pull_requests": pr_event_records[0].storage_id}))
+
+    return github_prs_updated_sensor
